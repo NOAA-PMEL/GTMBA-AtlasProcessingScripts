0676ad241644346f037afed72ab82f6766c80195
Daryn White <daryn.white@noaa.gov>
2020-10-27 14:50:10 -0700

    Quick updates & docstring for atlasRH.py

--- atlasRH.py
+++ atlasRH.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
+"""
+Script to check for & set RH values over 100% to 100%
+Also, this will set the flag falue to 3
+
+Use: atlasRH met124a.flg
+"""
 ## Author: Daryn White, daryn.white@noaa.gov
-## Last altered: 2020-08-26
+## Last altered: 2020-10-27
 import os
 import argparse
 import tao.atlas.flg
@@ -13,23 +19,23 @@ parser = argparse.ArgumentParser(
 parser.add_argument("fl", metavar="file", help="FLG file for processing")
 args = parser.parse_args()
 
-# Load file
+## Load file
 metFile = tao.atlas.flg.File(args.fl)
-# Move original file
+## Move original file
 os.rename(args.fl, args.fl + "_orig")
-# Load frame
+## Load frame
 metFrame = metFile.frame
 
-# Qualities go first
+## Qualities go first
 metFrame.loc[metFrame["HUM"]["-3"] > 100.00, ("Q", 3)] = 3
-# Set values
+## Set values
 metFrame.loc[metFrame["Q"][3] == 3, ("HUM", "-3")] = 100.00
 
-# Write file with altered data
+## Write file with altered data
 metFile.writeAtlas(
     metFrame,
     formats=[
-        "8.2",  # u
+        "7.2",  # u
         "7.2",  # v
         "7.2",  # spd
         "7.1",  # dir
