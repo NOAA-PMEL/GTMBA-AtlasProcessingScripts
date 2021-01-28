81242e4799407fd7688d3f1e60ff5254bdd95441
Daryn White <daryn.white@noaa.gov>
2021-01-27 16:33:40 -0800

    Added a check in atlasRH

--- atlasRH.py
+++ atlasRH.py
@@ -21,34 +21,40 @@ args = parser.parse_args()
 
 ## Load file
 metFile = tao.atlas.flg.File(args.fl)
-## Move original file
-os.rename(args.fl, args.fl + "_orig")
 ## Load frame
 metFrame = metFile.frame
 
-## Qualities go first
-metFrame.loc[metFrame["HUM"]["-3"] > 100.00, ("Q", 3)] = 3
-## Set values
-metFrame.loc[metFrame["Q"][3] == 3, ("HUM", "-3")] = 100.00
+## check for values over 100.00%
+if (metFrame["HUM"]["-3"] > 100.00).any():
+    ## Proceed w/altering the file
+    ## Move original file
+    os.rename(args.fl, args.fl + "_orig")
 
-## Write file with altered data
-metFile.writeAtlas(
-    metFrame,
-    formats=[
-        "7.2",  # u
-        "7.2",  # v
-        "7.2",  # spd
-        "7.1",  # dir
-        "7.2",  # at
-        "7.2",  # rh
-        "1.0",  # quals
-        "1.0",
-        "1.0",
-        "1.0",
-        "1.0",  # source
-        "1.0",
-        "1.0",
-        "1.0",
-    ],
-    output=args.fl,
-)
+    ## Qualities go first
+    metFrame.loc[metFrame["HUM"]["-3"] > 100.00, ("Q", 3)] = 3
+    ## Set values
+    metFrame.loc[metFrame["Q"][3] == 3, ("HUM", "-3")] = 100.00
+
+    ## Write file with altered data
+    metFile.writeAtlas(
+        metFrame,
+        formats=[
+            "7.2",  # u
+            "7.2",  # v
+            "7.2",  # spd
+            "7.1",  # dir
+            "7.2",  # at
+            "7.2",  # rh
+            "1.0",  # quals
+            "1.0",
+            "1.0",
+            "1.0",
+            "1.0",  # source
+            "1.0",
+            "1.0",
+            "1.0",
+        ],
+        output=args.fl,
+    )
+else:
+    print("No humidity values over 100.00%")
