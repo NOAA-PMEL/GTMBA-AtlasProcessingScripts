a34fcd3d17396a7578036819f61c772c9840cf99
Daryn White <daryn.white@noaa.gov>
2021-10-13 16:44:40 -0700

    Update atlasBuild.sh
    Beginning to use a bash script linter

--- atlasBuild.sh
+++ atlasBuild.sh
@@ -3,7 +3,7 @@
 # Author: Daryn A. White, daryn.white@noaa.gov -> Only for use w/in GTMBA
 
 ## About this script if fed nothing
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
   echo '
   atlasBuild: Clone and setup an Atlas data directory for processing.
 
@@ -16,23 +16,27 @@ if [ -z $1 ]; then
 fi
 
 ## Build out the needed functions and variables
-make_files () {
-  [ "1" = "$#" ] || { echo "Requires BUOYID name"; return 1; }
-  source ~/bin/tao/functions
-  D=($(splitdep $1))
-  calfile $1 | egrep -v '^#' > ${D[3]}.cal
-  nxram_setup $1
+make_files() {
+  [ "1" = "$#" ] || {
+    echo "Requires BUOYID name"
+    return 1
+  }
+  # shellcheck disable=SC1091
+  source /Users/white/bin/tao/functions
+  D=("$(splitdep "$1")")
+  calfile "$1" | grep -E -v '^#' >"${D[3]}".cal
+  nxram_setup "$1"
 }
 
 ## Actually go get things done
-git clone spectrum:/home/data/nxram/$1 -b processing
+git clone spectrum:/home/data/nxram/"$1" -b processing
 
-if [ $? -eq 0 ]; then
-  cd $1
-  docfile $1 > $1.doc
-  make_files $1
+if [ "$1" -eq 0 ]; then
+  cd "$1" || exit 1
+  docfile "$1" >"$1".doc
+  make_files "$1"
   echo "Ok, ${1} is built."
   code .
 else
-  echo 'Clone failed';
+  echo "Clone failed"
 fi
