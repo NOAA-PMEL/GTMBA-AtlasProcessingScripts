1b2e124752e4dca197a26b9cb9c82c22a6987e66
Daryn White <daryn.white@noaa.gov>
2021-11-03 10:53:37 -0700

    Enforced some formatting

--- SSC_ramshifts.sh
+++ SSC_ramshifts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Author: Daryn White, daryn.white@noaa.gov
 
-if [ -z $1 ] && [ -z $2 ]; then
+if [ -z "$1" ] && [ -z "$2" ]; then
     echo '
     SSC_ramshifts readjusts the SSC data column in temp, sal, cond, & dens files
     to undo erroneous shifts that happened during a deployment
@@ -21,45 +21,45 @@ if [ -z $1 ] && [ -z $2 ]; then
 fi
 
 if [[ $1 =~ ..\d\d\d[a-c] ]]; then
-    echo 'Need the deployment leg please'
-    echo 'Did you mean?\n $1a'
+    echo "Need the deployment leg please"
+    printf 'Did you mean?\n %sa' "$1"
     exit
 elif [[ $2 =~ ..\w.lst ]]; then
-    echo 'Need the output listfile please'
-    echo 'Did you mean?\n $2a'
+    echo "Need the output listfile please"
+    printf 'Did you mean?\n %sa' "$2"
     exit
-fi;
+fi
 
 id=${1: -4}
-file=$(find $2)
+file=$(find "$2")
 
 # check for ram_orig files and create them if they do not exist
 for fl in {temp,cond,sal,dens}; do
-    if [ -f $fl$id.ram_orig ]; then
+    if [ -f "$fl""$id".ram_orig ]; then
         echo "Original $fl$id.ram_orig exists"
     else
         echo "No original files"
-        cp $fl$id.ram $fl$id.ram_orig
-    fi;
-done;
+        cp "$fl""$id".ram "$fl""$id".ram_orig
+    fi
+done
 
-exec 5<$file
+exec 5<"$file"
 
-len=$(awk 'END { print NR }' $file)
+len=$(awk 'END { print NR }' "$file")
 
 # for i in $len; do TMP$i=$(mktemp); done;
 
-while read -u 5 d s; do
+while read -ru 5 d s; do
     l=$c
-    ((c=$c+1))
+    ((c = "$c" + 1))
     echo "adjustment date: $d | # of samples: $s | l=$l | c=$c"
     if [[ $c == 1 ]]; then
-        ram_shift --at=$d --col=1 --samples=$s temp$id.ram_orig > $c
+        ram_shift --at="$d" --col=1 --samples="$s" temp"$id".ram_orig >"$c"
     else
-        ram_shift --at=$d --col=1 --samples=$s $l > $c
-    fi;
-done;
+        ram_shift --at="$d" --col=1 --samples="$s" "$l" >"$c"
+    fi
+done
 
-cat $len > temp$id.ram
+cat "$len" >temp"$id".ram
 
-rm -f $(seq 1 $len)
+rm -f $(seq 1 "$len")
--- badA2increment.sh
+++ badA2increment.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 # Script to deal with improperly incremented subsurface modules from Atlas
 
-if [ -z $1 ] && [ -z $2 ] && [ -z $3 ]; then
+if [ -z "$1" ] && [ -z "$2" ] && [ -z "$3" ]; then
   echo '
   badA2increment copies and processes the bad file in a new directory.
-  
+
   Use: badA2increment [Serial #] [increm] [Deploy ID] [Type]
-        
+
         Serial #  = ONLY the serial number of the improperly deployed module
         increm    = the increment OF the instrument (120, 180, etc)
         Deploy ID = the id of the deployment, [ra|pi]###[a|b]
         Type      = type of files [ temp | sal | pres ]
         Cond #    = serial of the conductivity/pressure sensor, required for merging.
-  
-  Examples: 
+
+  Examples:
   badA2increment 11212 120 ra121a temp
   badA2increment 11212 120 ra121a sal 1211
   badA2increment 11212 120 ra121a pres 234882
@@ -21,112 +21,121 @@ if [ -z $1 ] && [ -z $2 ] && [ -z $3 ]; then
   exit 1
 fi
 
-if [ -z $1 ]; then
+if [ -z "$1" ]; then
   echo 'Need the serial number!'
   exit
-elif [ -z $2 ]; then
+elif [ -z "$2" ]; then
   echo 'Need the increment!'
   exit
-elif [ -z $3 ]; then
+elif [ -z "$3" ]; then
   echo 'Need the deployment!'
   exit
 elif [[ $3 =~ ..\d\d\d[a-c] ]]; then
   echo 'Need the deployment leg please'
-  echo 'Did you mean?\n $3a'
+  printf 'Did you mean?\n %sa' "$3"
   exit
 elif [[ $1 =~ [A-Za-z] ]]; then
   echo 'No letters in the serial number please'
   exit
 elif [[ $2 =~ [0-9]{2,3}\b ]]; then
-  echo 'Did you mean $2 increment?'
+  printf 'Did you mean %s increment?' "$2"
   exit
-elif [ $4 == 'temp' ]; then
-  file=$(find *$1.*)
-  calfile=$(find *.cal)
+elif [ "$4" == 'temp' ]; then
+  file=$(find "*$1.*")
+  calfile=$(find "*".cal)
   id=${3: -4}
   echo 'Creating directory...'
-  mkdir $1
-  mv $file $1/
+  mkdir "$1"
+  mv "$file" "$1"/
   echo 'Moving files, running processing programs...'
-  processA2Mod $3 ./ --cal $calfile
-  cd $1/
-  cp $file ${file}_orig
-  processA2Mod $3 $file --cal ../$calfile --inc $2
+  processA2Mod "$3" ./ --cal "$calfile"
+  cd "$1"/ || exit 5
+  cp "$file" "${file}"_orig
+  processA2Mod "$3" "$file" --cal ../"$calfile" --inc "$2"
   echo 'Cleaning files, moving up for merging...'
-  find ${4}${id}.ram | while read file; do
-    (head -n 5 $file; tail -n +6 $file | grep -e '\d\d\d\d\d\d\d\d\d\d000') > ${file}_${2}
-    mv ${file}_${2} ../
+  find "${4}""${id}".ram | while read -r file; do
+    (
+      head -n 5 "$file"
+      tail -n +6 "$file" | grep -e '\d\d\d\d\d\d\d\d\d\d000'
+    ) >"${file}"_"${2}"
+    mv "${file}"_"${2}" ../
   done
   cd ..
   echo 'Merging files...'
-  find ${4}${id}.ram | while read file; do
-    mv $file ${file}_orig
-    mergeA2ram $1 ${file}_orig ${file}_${2} > ${file}
-    mv ${file}_${2} $1/
+  find "${4}""${id}".ram | while read -r file; do
+    mv "$file" "${file}"_orig
+    mergeA2ram "$1" "${file}"_orig "${file}"_"${2}" >"${file}"
+    mv "${file}"_"${2}" "$1"/
   done
   echo 'Done.'
-elif [ $4 == 'pres' ]; then
-  if [ -z $5 ]; then
+elif [ "$4" == 'pres' ]; then
+  if [ -z "$5" ]; then
     echo 'Need the pressure sensor serial'
     exit
   fi
-  file=$(find *$1.*)
-  calfile=$(find *.cal)
+  file=$(find "*$1.*")
+  calfile=$(find "*".cal)
   id=${3: -4}
   echo 'Creating directory...'
-  mkdir $1
-  mv $file $1/
+  mkdir "$1"
+  mv "$file" "$1"/
   echo 'Moving files, running processing programs...'
-  processA2Mod $3 ./ --cal $calfile
-  cd $1/
-  cp $file ${file}_orig
-  processA2Mod $3 $file --cal ../$calfile --inc $2
+  processA2Mod "$3" ./ --cal "$calfile"
+  cd "$1"/ || exit 6
+  cp "$file" "${file}"_orig
+  processA2Mod "$3" "$file" --cal ../"$calfile" --inc "$2"
   echo 'Cleaning files, moving up for merging...'
-  find ${4}${id}.ram | while read file; do
-    (head -n 5 $file; tail -n +6 $file | grep -e '\d\d\d\d\d\d\d\d\d\d000') > ${file}_${2}
-    mv ${file}_${2} ../
+  find "${4}""${id}".ram | while read -r file; do
+    (
+      head -n 5 "$file"
+      tail -n +6 "$file" | grep -e '\d\d\d\d\d\d\d\d\d\d000'
+    ) >"${file}"_"${2}"
+    mv "${file}"_"${2}" ../
   done
   cd ..
   echo 'Merging files...'
-  find ${4}${id}.ram | while read file; do
-    mv $file ${file}_orig
-    mergeA2ram $5 ${file}_orig ${file}_${2} > ${file}
-    mv ${file}_${2} $1/
+  find "${4}""${id}".ram | while read -r file; do
+    mv "$file" "${file}"_orig
+    mergeA2ram "$5" "${file}"_orig "${file}"_"${2}" >"${file}"
+    mv "${file}"_"${2}" "$1"/
   done
   echo 'Done.'
-elif [ $4 == 'sal' ]; then
-  if [ -z $5 ]; then
+elif [ "$4" == 'sal' ]; then
+  if [ -z "$5" ]; then
     echo 'Need the conductivity cell number'
     exit
   fi
-  file=$(find *$1.*)
-  calfile=$(find *.cal)
+  file=$(find "*$1.*")
+  calfile=$(find "*".cal)
   id=${3: -4}
   echo 'Creating directory...'
-  mkdir $1
-  mv $file $1/
+  mkdir "$1"
+  mv "$file" "$1"/
   echo 'Moving files, running processing programs...'
-  processA2Mod $3 ./ --cal $calfile
-  cd $1/
-  cp $file ${file}_orig
-  processA2Mod $3 $file --cal ../$calfile --inc $2
+  processA2Mod "$3" ./ --cal "$calfile"
+  cd "$1"/ || exit 8
+  cp "$file" "${file}"_orig
+  processA2Mod "$3" "$file" --cal ../"$calfile" --inc "$2"
   echo 'Cleaning files, moving up for merging...'
-  find {cond${id}.ram,dens${id}.ram,sal${id}.ram,temp${id}.ram} | while read file; do
-    (head -n 5 $file; tail -n +6 $file | grep -e '\d\d\d\d\d\d\d\d\d\d000') > ${file}_${2}
-    mv ${file}_${2} ../
+  find {cond"${id}".ram,dens"${id}".ram,sal"${id}".ram,temp"${id}".ram} | while read -r file; do
+    (
+      head -n 5 "$file"
+      tail -n +6 "$file" | grep -e '\d\d\d\d\d\d\d\d\d\d000'
+    ) >"${file}"_"${2}"
+    mv "${file}"_"${2}" ../
   done
-  cd ..
+  cd .. || exit 7
   echo 'Merging salinity files...'
-  find {cond${id}.ram,dens${id}.ram,sal${id}.ram} | while read file; do
-    mv $file ${file}_orig
-    mergeA2ram $5 ${file}_orig ${file}_${2} > ${file}
-    mv ${file}_${2} $1/
+  find {cond"${id}".ram,dens"${id}".ram,sal"${id}".ram} | while read -r file; do
+    mv "$file" "${file}"_orig
+    mergeA2ram "$5" "${file}"_orig "${file}"_"${2}" >"${file}"
+    mv "${file}"_"${2}" "$1"/
   done
   echo 'Merging temperature file...'
-  find temp${id}.ram | while read file; do
-    mv $file ${file}_orig
-    mergeA2ram $1 ${file}_orig ${file}_${2} > ${file}
-    mv ${file}_${2} $1/
+  find temp"${id}".ram | while read -r file; do
+    mv "$file" "${file}"_orig
+    mergeA2ram "$1" "${file}"_orig "${file}"_"${2}" >"${file}"
+    mv "${file}"_"${2}" "$1"/
   done
   echo 'Done.'
 else
