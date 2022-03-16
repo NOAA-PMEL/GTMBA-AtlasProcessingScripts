80f6fab8a6e9f3af85986b9e32ec926ef63b13b9
Daryn White <daryn.white@noaa.gov>
2022-03-15 22:47:42 -0700

    Create tube_buffer_clean.py
    Created a script to add newline characters before any `CAFE` instance

---
+++ tube_buffer_clean.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+Script to clean up a bad/corrupted Atlas Tube file.
+"""
+
+import re
+from argparse import ArgumentParser as AP
+
+
+# Functions
+def is_binary(bytes):
+    "Boolean function for determing file as binary or hex"
+    textchars = bytearray({7, 8, 9, 10, 12, 13, 27} | set(range(0x20, 0x100)) - {0x7F})
+    return bool(bytes.translate(None, textchars))
+
+
+# Regex
+patt = r"(?<!\b)CAFE"
+subs = "\\nCAFE"
+
+# Parser
+parser = AP(
+    prog="tube_buffer_clean",
+    description="""
+        Script to attempt to clean a tube data dump, if corrupt.
+
+        This script only adds newlines in front of any CAFE
+        instances that are not at the beginning of a line.
+        """,
+)
+parser.add_argument("file", metavar="file", help="HEX or binary file to be cleaned")
+args = parser.parse_args()
+
+with open(args.file, "rb") as file:
+    if is_binary(file.read(1024)):
+        file.seek(0)
+        hexData = file.read().hex().upper()
+        outFile = file.name[:-3] + "hex_cleaned"
+    else:
+        file.seek(0)
+        hexData = file.read().decode().upper()
+        outFile = file.name + "_cleaned"
+
+    cleanData = re.sub(patt, subs, hexData, 0, re.MULTILINE)
+    with open(outFile, "w") as output:
+        output.write(cleanData)
