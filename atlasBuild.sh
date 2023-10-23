053b73ffc03b00881f272287a2eb0e1b72bb83ab
Daryn White <daryn.white@noaa.gov>
2023-10-23 16:17:16 -0700

    Update atlasBuild.sh
    Added better exits if `git clone` fails on initial call
    Added a `--noeditor` option to simply clone and setup deployment repos
    Better documentation on a `null` call

--- atlasBuild.sh
+++ atlasBuild.sh
@@ -10,9 +10,15 @@ if [ -z "$1" ]; then
   This is purely to make the setup process faster and get into processing data
   quicker. Use is simple, give the name of the directory.
 
-  Example: atlasBuild ra102
+  --noeditor: this option will stop VSCode from launching with the cloned repo open
+
+  Usage: atlasBuild DEPID [--noeditor]
+
+  Examples:
+    > atlasBuild ra102
+    > atlasBuild ra102 --noeditor
   '
-  exit 1
+  exit
 fi
 
 ## Build out the needed functions and variables
@@ -34,12 +40,14 @@ valid="[a-zA-Z][a-zA-Z][0-9]{3}$"
 if [[ ! "$1" =~ $valid ]]; then
   echo "BUOYID should be in the form aa### not $1"
 elif [[ "$1" =~ $valid ]]; then
-  git clone spectrum:/home/data/nxram/"$1" -b processing
+  git clone spectrum:/home/data/nxram/"$1" -b processing || exit 5
   cd "$1" || exit 1
   docfile "$1" >"$1".doc
   make_files "$1"
   echo "Ok, ${1} is built."
-  code .
+  if [[ ! "$2" =~ "--noeditor" ]]; then
+    code .
+  fi
 else
   echo "Clone failed"
 fi
