23b107e0ed91a110caa98e4f6bb19b4144c98ed8
Daryn White <daryn.white@noaa.gov>
2024-04-01 19:21:12 -0700

    Bash cleaning and stopping Pylint
    Pylint is proving to be too strict

--- splitdep.sh
+++ splitdep.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 dep=$1 pre="" num="" seg="" 
-if [[ "$(echo ${dep}|egrep '[0-9]+$')" != "" ]]
+if [[ "$(echo "${dep}"|egrep '[0-9]+$')" != "" ]]
 then
         dep="${dep}a" 
 fi
-if [[ "$(echo ${dep}|egrep '^[a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
+if [[ "$(echo "${dep}"|egrep '^[a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
 then
         pre=${dep:0:2} 
         num=${dep:2:${#dep}-3} 
         seg=${dep:0-1} 
-elif [[ "$(echo ${dep}|egrep '^[a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
+elif [[ "$(echo "${dep}"|egrep '^[a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
 then
         pre=${dep:0:3} 
         num=${dep:3:${#dep}-4} 
         seg=${dep:0-1} 
-elif [[ "$(echo ${dep}|egrep '^[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
+elif [[ "$(echo "${dep}"|egrep '^[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
 then
         pre=${dep:0:4} 
         num=${dep:4:${#dep}-5} 
@@ -23,4 +23,4 @@ then
 else
         error "Unable to recognize the deployment identifier!" || return 1
 fi
-echo "${pre} ${num} ${seg}" ${dep}
+echo "${pre} ${num} ${seg}" "${dep}"
