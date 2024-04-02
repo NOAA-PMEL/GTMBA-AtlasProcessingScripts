2637b544c9ca03171ce576ec0fc10f77b88ac53d
Daryn White <daryn.white@noaa.gov>
2024-04-01 19:27:46 -0700

    Bash linting

--- splitdep.sh
+++ splitdep.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 dep=$1 pre="" num="" seg="" 
-if [[ "$(echo "${dep}"|egrep '[0-9]+$')" != "" ]]
+if [[ "$(echo "${dep}"|grep -E '[0-9]+$')" != "" ]]
 then
         dep="${dep}a" 
 fi
-if [[ "$(echo "${dep}"|egrep '^[a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
+if [[ "$(echo "${dep}"|grep -E '^[a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
 then
         pre=${dep:0:2} 
         num=${dep:2:${#dep}-3} 
         seg=${dep:0-1} 
-elif [[ "$(echo "${dep}"|egrep '^[a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
+elif [[ "$(echo "${dep}"|grep -E '^[a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
 then
         pre=${dep:0:3} 
         num=${dep:3:${#dep}-4} 
         seg=${dep:0-1} 
-elif [[ "$(echo "${dep}"|egrep '^[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
+elif [[ "$(echo "${dep}"|grep -E '^[a-zA-Z][a-zA-Z][a-zA-Z][a-zA-Z][0-9]+[a-zA-Z]$')" != "" ]]
 then
         pre=${dep:0:4} 
         num=${dep:4:${#dep}-5} 
